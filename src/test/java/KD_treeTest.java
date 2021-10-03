f60f6f42db9d9b98a242af9e94cc9b92df096280
sdpy695uw325fyuv@gmail.com <sdpy695uw325fyuv@gmail.com>
2021-10-03 23:14:53 +0000

    trying to pull!

--- src/test/java/KD_treeTest.java
+++ src/test/java/KD_treeTest.java
@@ -18,6 +18,26 @@ public class KD_treeTest {
     assertEquals(90, root.getCoord(2), 0.01);
   }
 
+  @Test
+  public void testBigTree() {
+    File file = new File("data/project-1/bigKDTreeTest.json");
+    KD_tree tree = new KD_tree(3, file);
+    KD_node root = tree.getRoot_();
+    System.out.println(root.getCoord(0));
+    System.out.println(root.getLeft().getCoord(0));
+    System.out.println(root.getRight().getCoord(0));
+    System.out.println(root.getLeft().getLeft().getCoord(0));
+    System.out.println(root.getLeft().getRight().getCoord(0));
+    System.out.println(root.getRight().getLeft().getCoord(0));
 
+    assertEquals(2, root.getCoord(0), 0.01);
+    assertEquals(61, root.getCoord(1), 0.01);
+    assertEquals(40, root.getCoord(2), 0.01);
+    assertEquals(2, root.getLeft().getCoord(0), 0.01);
+    assertEquals(63, root.getLeft().getCoord(1), 0.01);
+    assertEquals(70, root.getLeft().getCoord(3), 0.01);
+    assertEquals(6, root.getRight().getLeft().getCoord(0), 0.01);
+    assertEquals(64, root.getRight().getLeft().getCoord(1), 0.01);
+  }
 
 }
